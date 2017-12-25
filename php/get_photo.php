cf37a47fc8ee9ae2a827ca960d8fc2b43734d16a
HayatoTsukiji <hayatotsukiji@gmail.com>
2017-12-25 10:23:45 +0900

    ファイル送信方法を非同期通信ajaxに変更

--- php/get_photo.php
+++ php/get_photo.php
@@ -1,7 +1,7 @@
 <?php
-
+//header('Content-Type: application/json; charset=utf-8');
 $photo = $_FILES['photo'];
-
+/*
 if ($photo['name'] != null) {
     $result = "ok";
 }
@@ -9,7 +9,12 @@ else{
     $result = "no";
 }
 
+
 echo $result;
 var_dump ($photo);
+*/
+$data = "ok";
 
+
+print json_encode($data);
 ?>
