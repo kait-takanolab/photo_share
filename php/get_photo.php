5ff50e2dae6b14ebc765d4cdf402cd619378308b
kohei1110 <s1421172@cco.kanagawa-it.ac.jp>
2017-12-25 10:36:44 +0900

    データベースに写真名を追加する部分を追加＆jsで写真名を受け取る部分を追加

--- php/get_photo.php
+++ php/get_photo.php
@@ -1,21 +1,35 @@
 <?php
 
+//データベース接続用phpをrequire
+require_once 'get_db.php';
+
 $photo = $_FILES['photo'];
 $photodata=$_FILES['photo']['tmp_name'];
 
 $photoname=$photo['name'];
-if ($photo['name'] != null) {
-    $result = "ok";
-}
-else{
-    $result = "no";
-}
+//if ($photo['name'] != null) {
+   // $result = "ok";
+//}
+//else{
+   // $result = "no";
+//}
 
 //echo $result;
 //var_dump ($photo);
 //var_dump ($photodata);
 
+//ファイルをimageフォルダ内に保存
 rename($photodata,"../images/$photoname");
+
+//db接続したPDOを格納
+$db=getsqlitedb();
+
+//photo_nameテーブルに保存した写真の名前を保存するステートメント
+$stmt=$db->prepare("INSERT INTO photo_name(name) VALUES(:photoname)");
+$stmt->bindvalue(":photoname", $photoname);
+
+//ステートメントの実行
+$stmt->execute();
 
 ?>
 
