90774e4772bb3c60fb019123b58157000b290d59
unblee <unblee@users.noreply.github.com>
2018-07-18 04:12:27 +0900

    DB初期化用のスクリプトを作成

--- june_version/php/get_db.php
+++ june_version/php/get_db.php
@@ -1,14 +1,17 @@
 <?php
 
-function getsqlitedb(){
- $sqlitedb = new PDO('sqlite:../db/photo_share_db.sqlite3');
+// sqlite3のDBファイル
+const DB_PATH = __DIR__ . "/../db/photo_share.sqlite3";
+
+function getsqlitedb()
+{
+    $sqlitedb = new PDO('sqlite:' . DB_PATH);
     mb_language("uni");
     mb_internal_encoding("utf-8");
     mb_http_input("auto");
     mb_http_output("utf-8");
     $sqlitedb->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sqlitedb->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
+    $sqlitedb->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-return $sqlitedb;
+    return $sqlitedb;
 }
- ?>
