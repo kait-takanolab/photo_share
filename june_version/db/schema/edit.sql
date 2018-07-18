b222954d46cb22fcad782073fd075dcb6d675572
unblee <unblee@users.noreply.github.com>
2018-07-18 18:34:47 +0900

    DBのスキーマ定義SQLを更新

--- june_version/db/schema/edit.sql
+++ june_version/db/schema/edit.sql
@@ -1,19 +1,32 @@
-PRAGMA foreign_keys=OFF;
-BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS `edit` (
-        `id`                INTEGER PRIMARY KEY AUTOINCREMENT,
-        `name`              TEXT NOT NULL,
-        `crop_size`         TEXT,
-        `edit_duration`     INTEGER,
-        `gaussian_filter`   INTEGER,
-        `contrast _filter`  INTEGER,
-        `grayscale_filter`  INTEGER,
-        `sepia_filter`      INTEGER,
-        `invert_filter`     INTEGER,
-        `hue_rotate_filter` INTEGER,
-        `lightness_filter`  INTEGER,
-        `saturation_filter` INTEGER,
-        `photo_id`          INTEGER NOT NULL
+PRAGMA foreign_keys = OFF;BEGIN TRANSACTION;CREATE TABLE IF NOT EXISTS `edit` (
+  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `name` TEXT NOT NULL,
+  `crop_size` TEXT,
+  `edit_duration` INTEGER,
+  `gaussian_filter` INTEGER,
+  `contrast _filter` INTEGER,
+  `grayscale_filter` INTEGER,
+  `sepia_filter` INTEGER,
+  `invert_filter` INTEGER,
+  `hue_rotate_filter` INTEGER,
+  `lightness_filter` INTEGER,
+  `saturation_filter` INTEGER,
+  `photo_id` INTEGER NOT NULL
 );
-INSERT INTO edit VALUES(1,'grayscale_cat',NULL,NULL,NULL,NULL,100,NULL,NULL,NULL,NULL,NULL,1);
-COMMIT;
+INSERT INTO
+  edit
+VALUES(
+    1,
+    'grayscale_cat.png',
+    '640,360',
+    0,
+    0,
+    0,
+    100,
+    0,
+    0,
+    0,
+    100,
+    100,
+    1
+  );COMMIT;
--- june_version/db/schema/filter_preset.sql
+++ june_version/db/schema/filter_preset.sql
@@ -1,16 +1,15 @@
-PRAGMA foreign_keys=OFF;
-BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS `filter_preset` (
-        `id`                INTEGER PRIMARY KEY AUTOINCREMENT,
-        `name`              TEXT NOT NULL,
-        `gaussian_filter`   INTEGER,
-        `contrast _filter`  INTEGER,
-        `grayscale_filter`  INTEGER,
-        `sepia_filter`      INTEGER,
-        `invert_filter`     INTEGER,
-        `hue_rotate_filter` INTEGER,
-        `lightness_filter`  INTEGER,
-        `saturation_filter` INTEGER
+PRAGMA foreign_keys = OFF;BEGIN TRANSACTION;CREATE TABLE IF NOT EXISTS `filter_preset` (
+  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `name` TEXT NOT NULL,
+  `gaussian_filter` INTEGER,
+  `contrast _filter` INTEGER,
+  `grayscale_filter` INTEGER,
+  `sepia_filter` INTEGER,
+  `invert_filter` INTEGER,
+  `hue_rotate_filter` INTEGER,
+  `lightness_filter` INTEGER,
+  `saturation_filter` INTEGER
 );
-INSERT INTO filter_preset VALUES(1,'sepia',NULL,NULL,NULL,100,NULL,NULL,NULL,NULL);
-COMMIT;
+INSERT INTO
+  filter_preset
+VALUES(1, 'sepia', 0, 0, 0, 100, 0, 0, 100, 100);COMMIT;
